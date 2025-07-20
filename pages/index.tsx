bf02861af9c6f202100195577340aa09b7d6dd62
Keagee9 <Kingsleyfrancis.kalu@gmail.com>
2025-07-20 19:02:37 +0100

    week2

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage: React.FC = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">Welcome to the ALX Listing App</h1>
+    </div>
+  );
+};
+
+export default HomePage;
